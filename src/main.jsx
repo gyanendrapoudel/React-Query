69b3e1ec78ba77a1e502cd7c554bd5ef4c68bf35
Gyanendra <gyanen56@gmail.com>
2024-07-19 16:41:56 +1000

    first request to http://localhost:5000/api/tasks

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
-<QueryClientProvider client={QueryClient}>  
+<QueryClientProvider client={queryClient}>  
 <App />
 </QueryClientProvider>
 
