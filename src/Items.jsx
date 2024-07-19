2229129fc6b45d55a54299b39f630ec71385b809
Gyanendra <gyanen56@gmail.com>
2024-07-19 17:25:42 +1000

    error handling in react query

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,12 +2,22 @@ import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
 import customFetch from './utils';
 const Items = ({ items }) => {
-  const {data, isLoading} = useQuery({
+  const {data, isLoading, isError, error} = useQuery({
     queryKey:['tasks'],
-    queryFn:()=>customFetch.get("/")
+    queryFn:()=>customFetch.get("/something")
   })
   if(isLoading){
-    return <div style={{marginTop:'2rem'}}>...Loading</div>
+    return <div style={{ marginTop: '2rem' }}>Loading...</div>
+  }
+  // react query error
+  // if(isError){
+  //   return <div style={{ marginTop: '2rem' }}>There was an error...</div>
+
+  // }
+  // axios error
+  if(error){
+    return <div style={{ marginTop: '2rem' }}>{error.message}</div>
+  
   }
 
   return (
