452dda9d7c339507c13d89734a21c9d6887079bf
Gyanendra <gyanen56@gmail.com>
2024-07-21 12:45:14 +1000

    edit task

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,10 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient()
+  const { mutate: editTask } = useMutation({
+    mutationFn: (id) => customFetch.patch(`/${id}`, { isDone: item.isDone }),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] })
+      toast.success(data.data.msg)
+    },
+    onError: (error) => {
+      toast.error(error.response.data)
+    },
+  })
+  const handleChange=(id)=>{
+   item.isDone=!item.isDone
+   editTask(id)
+   
+  }
   return (
     <div className='single-item'>
       <input
         type='checkbox'
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={()=>handleChange(item.id)}
       />
       <p
         style={{
